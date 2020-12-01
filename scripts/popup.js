792e8f6904269f08e74acb46dfa32dc52709db04
moscowx1 <moscowx1@mail.ru>
2020-12-01 19:17:13 +0500

    добавление элемента через js

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,33 +1,68 @@
 let popup = document.querySelector(".popup");
+let popupTitle = popup.querySelector('.popup__title');
 
 let form = popup.querySelector("form");
 
-let nameInput = popup.querySelector('input[name="name"]')
-let descInput = popup.querySelector('input[name="desc"]')
+let firstInput = popup.querySelector('input[name="firstInput"]')
+let secondInput = popup.querySelector('input[name="secondInput"]')
 
 let name = document.querySelector(".profile__name");
 let desc = document.querySelector(".profile__desc");
 
-let openBtn = document.querySelector(".profile__edit-button");
+let openEditProfileBtn = document.querySelector(".profile__edit-button");
+let openAddPicBtn = document.querySelector(".profile__add-button");
 let closeBtn = document.querySelector(".popup__close");
+
+let handleFunc = () => {};
 
 const HidePopup = () => {
   popup.classList.remove("popup_opened");
 }
 
 const OpenPopup = () => {
-  nameInput.value = name.textContent;
-  descInput.value = desc.textContent;
   popup.classList.add("popup_opened");
 }
 
 const Submit = (evt) => {
   evt.preventDefault();
-  name.textContent = nameInput.value;
-  desc.textContent = descInput.value;
+  handleFunc();
   HidePopup();
+};
+
+const OpenEditProfileForm = () => {
+  popupTitle.textContent = "Редактировать профиль";
+  firstInput.value = name.textContent;
+  secondInput.value = desc.textContent;
+  handleFunc = HandleEditProfile;
+  OpenPopup();
+}
+
+const HandleEditProfile = () => {
+  name.textContent = firstInput.value;
+  desc.textContent = secondInput.value;
+}
+
+const OpenAddPicForm = () => {
+  popupTitle.textContent = "Новое место";
+  handleFunc = HandleAddPic;
+  OpenPopup();
+}
+
+const HandleAddPic = () => {
+  const name = firstInput.value;
+  const url = secondInput.value;
+
+  let elem = document.querySelector("#element-template").content;
+  let newEl = elem.cloneNode(true);
+
+  newEl.querySelector(".element__img").src = url;
+  newEl.querySelector(".element__img").alt = `Изображение ${name}`;
+  newEl.querySelector(".element__name").textContent = name;
+
+  document.querySelector(".elements").prepend(newEl);
 }
 
 form.addEventListener("submit", Submit);
-openBtn.addEventListener("click", OpenPopup);
+openEditProfileBtn.addEventListener("click", OpenEditProfileForm);
+openAddPicBtn.addEventListener("click", OpenAddPicForm);
 closeBtn.addEventListener("click", HidePopup);
